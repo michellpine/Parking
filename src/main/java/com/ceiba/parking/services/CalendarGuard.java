44b3ed3e6c489c3826b89865a2145a37e8b79b2c
Michell Pineda <michellpine@hotmail.com>
2018-02-07 12:46:56 -0500

    calculando horas

--- src/main/java/com/ceiba/parking/services/CalendarGuard.java
+++ src/main/java/com/ceiba/parking/services/CalendarGuard.java
@@ -1,12 +1,25 @@
-package com.ceiba.parking.services;
+package com.ceiba.parking.domain;
 
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CalendarGuard {
+    TimeUnit timeUnit;
 
     public int getActualDay(){
-        return java.util.Calendar.getInstance().get(java.util.Calendar.DAY_OF_WEEK);
+        return Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+    }
+
+    public void getActualHour(Date dateArrive, Date dateOut){
+        long diffInDays = (dateOut.getTime() - dateArrive.getTime())/(1000);
+        System.out.println("minutos: "+ diffInDays);
     }
 
 }
+
