439336081a5c6bdd43368dccdb0b16b4795ec97b
Michell <michellpine@hotmail.com>
2018-02-12 14:17:18 -0500

    haciendo test

--- src/test/java/com/ceiba/parking/domain/ParkingTicketTest.java
+++ src/test/java/com/ceiba/parking/domain/ParkingTicketTest.java
@@ -1,32 +1,30 @@
 package com.ceiba.parking.domain;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+import static com.ceiba.parking.builder.VehicleTestDataBuilder.aVehicle;
 
 public class ParkingTicketTest {
 
-    ParkingTicket parkingTicket;
 
-    @Before
-    public void setUp() throws Exception {
-        parkingTicket = Mockito.mock(ParkingTicket.class);
-    }
-
-    /*@Test
-    public void addCar() {
-        Car car = aCar()
-                .withLicense("XCD123")
-                .withType(VehicleType.CAR)
-                .withIsParking(true)
-                .build();
-
-        parkingTicket.addCar(car);
-        assertEquals(car, parkingTicket.getCar());
-
-    }*/
 
     @Test
-    public void addMotorcycle() {
+    public void addVehicle(){
+        //Arrange
+        ParkingTicket parkingTicket = new ParkingTicket();
+        Vehicle car = aVehicle()
+                .withLicense("ACD123")
+                .withType(VehicleType.CAR)
+                .build();
+
+        parkingTicket.addVehicle(car);
+
+        Vehicle ticket = parkingTicket.getVehicle();
+
+        assertEquals(car, ticket);
     }
+
+
 }
